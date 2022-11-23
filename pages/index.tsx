f33a64dd381003228d8786786ce3ffde671120fc
Yasu <mahata777+github@gmail.com>
2022-11-23 22:55:00 +0900

    Switch to another reset-css framework

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 // import 'reset-css'
+import '@acab/reset.css';
 import styles from '../styles/Home.module.scss'
 
 export default function Home() {
