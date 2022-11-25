d95cf4e1bcae614c99c8a5a0b9337cfe4d4f38f3
Yasu <mahata777+github@gmail.com>
2022-11-25 17:26:07 +0900

    Fix tests

---
+++ __tests__/pages/index.test.tsx
@@ -0,0 +1,24 @@
+import Home from "../../pages";
+import {render, screen} from "@testing-library/react";
+
+const dummyAllPostData = [{
+  id: "id1",
+  title: "title1",
+  date: "2021-01-01",
+}, {
+  id: "id2",
+  title: "title2",
+  date: "2021-01-02",
+}];
+
+describe('index', () => {
+  it('foo', () => {
+    expect(1).toBe(1);
+  });
+
+  it('render', () => {
+    render(<Home allPostsData={dummyAllPostData} />);
+
+    expect(screen.getByText('自己紹介をここに書くんだって。')).toBeInTheDocument();
+  });
+});
