d4fdc54b393145dc71511cd63807d48dabf93e9b
Yasunori MAHATA <mahata777@gmail.com>
2025-01-28 11:52:14 +0900

    💚 Fix conf to make it buildable

--- testing/createJestConfig.ts
+++ testing/createJestConfig.ts
@@ -1,26 +1,28 @@
-import * as path from 'path';
+import path from "node:path";
 
-import nextJest from 'next/jest';
+import nextJest from "next/jest";
 
-import { esModules } from '../next.base';
+import { esModules } from "../next.base";
 
-const createJestConfig = nextJest({ dir: path.join(__dirname, '../') });
+const createJestConfig = nextJest({ dir: path.join(__dirname, "../") });
 
 interface JestConfig {
-  transformIgnorePatterns: string[];
+	transformIgnorePatterns?: string[];
 }
 
 // we have to use this override since next/js hard-codes node_modules in transformIgnorePatterns
-export default async function overriddenConfig(_config: any) {
-  return async function defaultExport() {
-    const config: JestConfig = await createJestConfig(_config)();
-    config.transformIgnorePatterns = [
-      '/.next/',
-      // ignore all ESM modules
-      `/node_modules/(?!(${esModules.join('|')}))(.*)`,
-      // CSS modules are mocked so they don't need to be transformed
-      '^.+\\.module\\.(css|sass|scss)$'
-    ];
-    return config;
-  };
+export default async function overriddenConfig(
+	_config: Record<string, unknown>,
+) {
+	return async function defaultExport() {
+		const config: JestConfig = await createJestConfig(_config)();
+		config.transformIgnorePatterns = [
+			"/.next/",
+			// ignore all ESM modules
+			`/node_modules/(?!(${esModules.join("|")}))(.*)`,
+			// CSS modules are mocked so they don't need to be transformed
+			"^.+\\.module\\.(css|sass|scss)$",
+		];
+		return config;
+	};
 }
