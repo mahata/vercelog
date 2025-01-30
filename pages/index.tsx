75a2afd113917adfbc8a36ce56577d59d14c8299
Yasunori MAHATA <mahata777@gmail.com>
2025-01-30 11:18:31 +0900

    ✨ Add pagination

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,18 @@ import Head from "next/head";
 import Link from "next/link";
 
 import "@acab/reset.css";
-import DateComponent from "../components/date";
-import Layout, { siteTitle } from "../components/layout";
-import { type PostData, getSortedPostsData } from "../lib/posts";
-import utilStyles from "../styles/utils.module.scss";
+import DateComponent from "@/components/date";
+import Layout, { siteTitle } from "@/components/layout";
+import { type PostData, getSortedPostsData } from "@/lib/posts";
+import utilStyles from "@/styles/utils.module.scss";
 
 type Props = {
   allPostsData: PostData[];
+  currentPage: number;
+  totalPages: number;
 };
 
-export default function Home({ allPostsData }: Props) {
+export default function Home({ allPostsData, currentPage, totalPages }: Props) {
   return (
     <Layout home>
       <Head>
@@ -30,16 +32,38 @@ export default function Home({ allPostsData }: Props) {
             </li>
           ))}
         </ul>
+        <div className={utilStyles.pagination}>
+          {currentPage > 1 && (
+            <Link href={`/?page=${currentPage - 1}`}>前のページ</Link>
+          )}
+          <span>
+            {currentPage} / {totalPages}
+          </span>
+          {currentPage < totalPages && (
+            <Link href={`/?page=${currentPage + 1}`}>次のページ</Link>
+          )}
+        </div>
       </section>
     </Layout>
   );
 }
 
-export async function getStaticProps() {
-  const allPostsData = await getSortedPostsData();
+export async function getServerSideProps({ query }: { query: { page?: string } }) {
+  const POSTS_PER_PAGE = 10;
+  const currentPage = query.page ? Number.parseInt(query.page) : 1;
+
+  const allPosts = await getSortedPostsData();
+  const totalPages = Math.ceil(allPosts.length / POSTS_PER_PAGE);
+
+  const startIndex = (currentPage - 1) * POSTS_PER_PAGE;
+  const endIndex = startIndex + POSTS_PER_PAGE;
+  const allPostsData = allPosts.slice(startIndex, endIndex);
+
   return {
     props: {
       allPostsData,
+      currentPage,
+      totalPages,
     },
   };
 }
