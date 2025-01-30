5732c200d1dc27be69e477fc652e24b201462b66
Yasunori MAHATA <mahata777@gmail.com>
2025-01-30 14:50:57 +0900

    ✨ Let the logo always leads users to home

--- components/layout.tsx
+++ components/layout.tsx
@@ -19,22 +19,21 @@ export default function Layout({ children, home }: Props) {
     <div className={styles.container}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <link rel="alternate" href="https://mahata.vercel.app/feed" type="application/rss+xml" title={siteTitle} />
+        <link rel="alternate" href="https://vercelog.mahata.org/feed" type="application/rss+xml" title={siteTitle} />
         <meta name="description" content="ソフトウェアに関する話だったり、そうでなかったり。" />
         <meta name="og:title" content={siteTitle} />
       </Head>
       <header className={styles.header}>
-        {home ? (
-          <>
-            <Image priority src="/images/profile.png" className={utilStyles.borderCircle} height={64} width={64} alt="Profile Icon" />
-          </>
-        ) : (
-          <>
-            <Link href="/">
-              <Image priority src="/images/profile.png" className={utilStyles.borderCircle} height={48} width={48} alt="Profile Icon" />
-            </Link>
-          </>
-        )}
+        <Link href="/">
+          <Image
+            priority
+            src="/images/profile.png"
+            className={utilStyles.borderCircle}
+            height={home ? 96 : 72}
+            width={home ? 96 : 72}
+            alt="Profile Icon"
+          />
+        </Link>
       </header>
 
       <main>{children}</main>
