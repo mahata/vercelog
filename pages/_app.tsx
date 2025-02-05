82ebce349d9f2a3bf7500ca9132c0163171e44e6
Yasunori MAHATA <mahata777@gmail.com>
2025-02-05 16:30:43 +0900

    🐛 Fix to make Cloudflare Web Tracking work as expected

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Script
+        id="cloudflare-analytics"
+        strategy="afterInteractive"
+        src="https://static.cloudflareinsights.com/beacon.min.js"
+        data-cf-beacon={`{"token": "${process.env.NEXT_PUBLIC_CLOUDFLARE_ANALYTICS_TOKEN}"}`}
+      />
+    </>
+  );
 }
