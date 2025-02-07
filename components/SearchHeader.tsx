a79bf2d53fcda1ce46a524fbe1a0f08aaeaa32db
Yasunori MAHATA <mahata777@gmail.com>
2025-02-07 13:57:50 +0900

    ✅ Fix lint errors

--- components/SearchHeader.tsx
+++ components/SearchHeader.tsx
@@ -3,8 +3,8 @@ import type { Hit } from "instantsearch.js";
 import type React from "react";
 import { Highlight, Hits, InstantSearch, SearchBox, useSearchBox } from "react-instantsearch";
 import "instantsearch.css/themes/satellite.css";
+import styles from "@/components/SearchHeader.module.scss";
 import Link from "next/link";
-import styles from '@/components/SearchHeader.module.scss';
 
 const searchClient = algoliasearch(process.env.NEXT_PUBLIC_ALGOLIA_APP_ID ?? "", process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY ?? "");
 
@@ -19,10 +19,10 @@ const HitComponent = ({ hit }: { hit: HitType }) => {
   return (
     <Link href={hit.path}>
       <article>
-        <h2>
+        <h2 className={styles.title}>
           <Highlight attribute="title" hit={hit} />
         </h2>
-        <p>
+        <p className={styles.content}>
           <Highlight attribute="content" hit={hit} />
         </p>
       </article>
@@ -52,7 +52,7 @@ export default function SearchHeader() {
     <div className="search-header">
       <InstantSearch indexName="articles" searchClient={searchClient} routing={true}>
         <div className="search-container">
-          <div className={styles['custom-searchbox']}>
+          <div className={styles.customSearchbox}>
             <SearchBox />
           </div>
           <HitsWithQuery hitComponent={HitComponent} />
